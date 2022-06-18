8c72582680b820e60ff4c74a10e16ec7605fa598
Andy Candy <anindya20024@iiitd.ac.in>
2022-06-19 02:45:58 +0530

    moved colorizer

--- lua/colorschemes-config/tokyonight.lua
+++ lua/colorschemes-config/tokyonight.lua
@@ -1,4 +1,3 @@
-require('colorizer').setup()
 vim.cmd[[
 let g:tokyonight_colors = {
     \ 'bg' : '#282c34'
--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,10 +1,12 @@
 local set = vim.opt
 
+require('colorizer').setup()
 set.expandtab = true
 set.number = true
 set.shiftwidth = 4
 set.si = true
 set.ai = true
+set.ic = true
 set.mouse = a
 set.smartcase = true
 set.scrolloff = 5
