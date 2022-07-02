33b5e55707d78a5e96c0c05066fd261c56743c18
Andy Candy <anindya20024@iiitd.ac.in>
2022-07-02 12:32:55 +0530

    commentmig

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,5 +1,6 @@
 vim.cmd([[
     nnoremap cpp :w <bar> :te bash bar %:r <CR>i
-    nnoremap q :q <CR>
+    nnoremap q :q <CR>                  
     inoremap <Home> <Esc>0wi
+    inoremap <C-_> <Esc>0wi// <Esc><S-A>
 ]])
