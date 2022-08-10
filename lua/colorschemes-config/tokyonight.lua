a96bd01be8e40741ed3c2cf089dbb7675ef9ff6e
Andy Candy <anindya20024@iiitd.ac.in>
2022-08-10 17:58:52 +0530

    Transparency

--- lua/colorschemes-config/tokyonight.lua
+++ lua/colorschemes-config/tokyonight.lua
@@ -1,5 +1,19 @@
+-- vim.cmd[[
+-- let g:tokyonight_colors = {
+--    \ 'bg' : '#282c34'
+--\ }
+vim.cmd[[colorscheme tokyonight]]
+vim.g.tokyonight_colors = {
+    bg = '#282c34'
+}
+vim.g.tokyonight_transparent = vim.g.transparent_enabled
 vim.cmd[[
-let g:tokyonight_colors = {
-    \ 'bg' : '#282c34'
-\ }
-colorscheme tokyonight]]
+hi Normal guibg=none ctermbg=none
+hi LineNr guibg=none ctermbg=none
+hi Folded guibg=none ctermbg=none
+hi NonText guibg=none ctermbg=none
+hi SpecialKey guibg=none ctermbg=none
+hi VertSplit guibg=none ctermbg=none
+hi SignColumn guibg=none ctermbg=none
+hi EndOfBuffer guibg=none ctermbg=none
+]]
